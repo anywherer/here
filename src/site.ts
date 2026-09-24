60b994f6e8f0d28b32ba72a39cac743cb4789ccd
i <i>
2026-09-24 19:19:48 +1000

    ok

---
+++ src/site.ts
@@ -0,0 +1,6 @@
+export const SITE = {
+  title: 'here',
+  description:
+    'Here is a little place, somewhere lonely and cute, in a world big and complicated. Welcome to some of my writings, big and small.',
+  fallbackUrl: 'https://somewherer.com/',
+};
